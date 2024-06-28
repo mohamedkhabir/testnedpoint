7c1868efd4927c429a0dac2de3a3767d92c1e007
camelsoft <admin@camel-soft.com>
2024-06-28 21:25:38 +0100

    origin

---
+++ src/main/java/com/testproject/testendpoint/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.testproject.testendpoint;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests(authorizeRequests ->
+                        authorizeRequests.anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth2ResourceServer ->
+                        oauth2ResourceServer.jwt()
+                );
+
+        return http.build();
+    }
+}
---
+++ src/test/java/com/testproject/testendpoint/TestEndpointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testproject.testendpoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestEndpointApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
